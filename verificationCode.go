807dce55472ffcabb8b5782263dc5d4978720da0
lvliangjun <zh1@vip.qq.com>
2025-04-02 15:15:31 +0800

    1

--- verificationCode.go
+++ verificationCode.go
@@ -2,6 +2,7 @@ package verificationCode
 
 import (
 	"context"
+	"errors"
 	baseError "github.com/go-estar/base-error"
 	"github.com/go-estar/redis"
 	"github.com/thoas/go-funk"
@@ -9,7 +10,7 @@ import (
 )
 
 var (
-	ErrorVerificationCodeCreateId   = baseError.NewSystemCode("2701", "id and code can't be empty")
+	ErrorVerificationCodeIdEmpty    = baseError.NewSystemCode("2701", "id can't be empty")
 	ErrorVerificationCodeInterval   = baseError.NewCode("2702", "请在[%s]秒后再试")
 	ErrorVerificationCodeVerifyId   = baseError.NewSystemCode("2703", "id can't be empty")
 	ErrorVerificationCodeVerifyCode = baseError.NewSystemCode("2704", "code can't be empty")
@@ -17,11 +18,130 @@ var (
 	ErrorVerificationCodeVerify     = baseError.NewCode("2706", "验证码错误")
 )
 
-type VerificationCode struct {
+type Config struct {
 	Redis        *redis.Redis
 	Name         string
+	Chars        string
+	Length       int
 	ExpireTime   int
 	IntervalTime int
+}
+
+func New(c *Config) *VerificationCode {
+	if c == nil {
+		panic("config必须设置")
+	}
+	if c.Name == "" {
+		panic("Name必须设置")
+	}
+	if c.Redis == nil {
+		panic("Redis必须设置")
+	}
+	if c.Chars == "" {
+		c.Chars = "012346789"
+	}
+	if c.Length == 0 {
+		c.Length = 6
+	}
+	if c.ExpireTime == 0 {
+		c.ExpireTime = 300
+	}
+	if c.IntervalTime == 0 {
+		c.IntervalTime = 60
+	}
+	return &VerificationCode{c}
+}
+
+type VerificationCode struct {
+	*Config
+}
+type CreateConfig struct {
+	Code      string
+	MustRegen bool
+}
+type CreateOption func(*CreateConfig)
+
+func CreateWithCode(val string) CreateOption {
+	return func(c *CreateConfig) {
+		c.Code = val
+	}
+}
+func CreateWithMustRegen() CreateOption {
+	return func(c *CreateConfig) {
+		c.MustRegen = true
+	}
+}
+
+func (vc *VerificationCode) Create(id string, opts ...CreateOption) (string, error) {
+	if id == "" {
+		return "", ErrorVerificationCodeIdEmpty
+	}
+
+	conf := &CreateConfig{
+		Code:      "",
+		MustRegen: false,
+	}
+	for _, apply := range opts {
+		apply(conf)
+	}
+
+	remainTime, err := vc.Redis.TTL(context.Background(), vc.Name+":i:"+id).Result()
+	if err != nil {
+		return "", err
+	}
+	if remainTime > 0 {
+		return "", ErrorVerificationCodeInterval.Clone().SetMsgArgs(remainTime)
+	}
+
+	code := ""
+	if conf.Code != "" {
+		code = conf.Code
+	} else {
+		if !conf.MustRegen {
+			val, err := vc.Redis.Get(context.Background(), vc.Name+":"+id).Result()
+			if err != nil && !errors.Is(err, redis.Nil) {
+				return "", err
+			}
+			if val != "" {
+				code = val
+			}
+		}
+		if code == "" {
+			code = funk.RandomString(vc.Length, []rune(vc.Chars))
+		}
+	}
+
+	ctx := context.Background()
+	multi := vc.Redis.TxPipeline()
+	multi.Set(ctx, vc.Name+":"+id, code, time.Duration(vc.ExpireTime)*time.Second)
+	multi.Set(ctx, vc.Name+":i:"+id, time.Now(), time.Duration(vc.IntervalTime)*time.Second)
+	_, err = multi.Exec(ctx)
+
+	return code, nil
+}
+
+func (vc *VerificationCode) Verify(id string, code string) error {
+	if id == "" {
+		return ErrorVerificationCodeVerifyId
+	}
+	if code == "" {
+		return ErrorVerificationCodeVerifyCode
+	}
+
+	val, err := vc.Redis.Get(context.Background(), vc.Name+":"+id).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return ErrorVerificationCodeExpired
+		}
+		return err
+	}
+
+	if val != code {
+		return ErrorVerificationCodeVerify
+	}
+
+	vc.Remove(id)
+	return nil
 }
 
 func (vc *VerificationCode) Remove(id string) error {
@@ -31,62 +151,4 @@ func (vc *VerificationCode) Remove(id string) error {
 	multi.Del(ctx, vc.Name+":i:"+id)
 	_, err := multi.Exec(ctx)
 	return err
-}
-
-func (vc *VerificationCode) Create(id string, code string) error {
-	if id == "" || code == "" {
-		return ErrorVerificationCodeCreateId
-	}
-
-	remainTime, err := vc.Redis.TTL(context.Background(), vc.Name+":i:"+id).Result()
-	if err != nil {
-		return err
-	}
-
-	if remainTime > 0 {
-		return ErrorVerificationCodeInterval.Clone().SetMsgArgs(remainTime)
-	}
-
-	ctx := context.Background()
-	multi := vc.Redis.TxPipeline()
-	multi.Set(ctx, vc.Name+":"+id, code, time.Duration(vc.ExpireTime)*time.Second)
-	multi.Set(ctx, vc.Name+":i:"+id, time.Now(), time.Duration(vc.IntervalTime)*time.Second)
-	_, err = multi.Exec(ctx)
-
-	return err
-}
-
-func (vc *VerificationCode) CreateCode(id string, length int) (string, error) {
-	code := funk.RandomString(length, []rune("012346789"))
-	if err := vc.Create(id, code); err != nil {
-		return "", err
-	}
-	return code, nil
-}
-
-func (vc *VerificationCode) Verify(id string, code string) error {
-
-	if id == "" {
-		return ErrorVerificationCodeVerifyId
-	}
-	if code == "" {
-		return ErrorVerificationCodeVerifyCode
-	}
-
-	old, err := vc.Redis.Get(context.Background(), vc.Name+":"+id).Result()
-
-	if err != nil {
-		if err == redis.Nil {
-			return ErrorVerificationCodeExpired
-		}
-		return err
-	}
-
-	if old != code {
-		return ErrorVerificationCodeVerify
-	}
-
-	vc.Remove(id)
-
-	return nil
 }
--- verificationCode_test.go
+++ verificationCode_test.go
@@ -3,26 +3,74 @@ package verificationCode
 import (
 	"github.com/go-estar/redis"
 	"testing"
+	"time"
 )
 
-func TestName(t *testing.T) {
-	var TestCode = &VerificationCode{
-		Redis: redis.New(&redis.Config{
-			Addr:     "127.0.0.1:6379",
-			Password: "GBkrIO9bkOcWrdsC",
-		}),
-		Name:         "test-code",
-		ExpireTime:   600,
-		IntervalTime: 60,
-	}
-	code, err := TestCode.CreateCode("a", 6)
+var TestCode = New(&Config{
+	Redis: redis.New(&redis.Config{
+		Addr:     "127.0.0.1:6379",
+		Password: "GBkrIO9bkOcWrdsC",
+	}),
+	Name:         "test-code",
+	ExpireTime:   600,
+	IntervalTime: 60,
+})
+
+func TestCreate(t *testing.T) {
+	code1, err := TestCode.Create("a")
 	if err != nil {
 		t.Fatal(err)
 	}
-	code, err = TestCode.CreateCode("a", 6)
+	t.Log("code1", code1)
+	time.Sleep(time.Minute)
+	code2, err := TestCode.Create("a")
 	if err != nil {
 		t.Fatal(err)
 	}
+	t.Log("code2", code2)
+}
+
+func TestCreateWithMustRegen(t *testing.T) {
+	code1, err := TestCode.Create("a")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("code1", code1)
+	time.Sleep(time.Minute)
+	code2, err := TestCode.Create("a", CreateWithMustRegen())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("code2", code2)
+}
+
+func TestCreateWithCode(t *testing.T) {
+	code1, err := TestCode.Create("a", CreateWithCode("1234"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("code1", code1)
+	time.Sleep(time.Minute)
+	code2, err := TestCode.Create("a")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("code2", code2)
+}
+
+func TestVerify(t *testing.T) {
+	if err := TestCode.Verify("a", "1234"); err != nil {
+		t.Fatal(err)
+	}
+	t.Log("Succeed")
+}
+
+func TestCreateVerify(t *testing.T) {
+	code, err := TestCode.Create("a")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("code", code)
 	if err := TestCode.Verify("a", code); err != nil {
 		t.Fatal(err)
 	}
